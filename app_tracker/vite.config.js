325a18ce3c591d5df527834b9c038e2eb7e293ef
chasehaye <chasehayejoc@gmail.com>
2024-05-21 22:37:25 -0700

    vite config mod

--- app_tracker/vite.config.js
+++ app_tracker/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
     },
   },
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tailwindcss('./tailwind.config.js'),
+  ],
+});

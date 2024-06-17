a999289d0432d0da088e91e6b5fb861de5413ef8
chasehaye <chasehayejoc@gmail.com>
2024-06-17 12:03:09 -0700

    pagination on index

--- app_tracker/src/JobListPage.jsx
+++ app_tracker/src/JobListPage.jsx
@@ -1,28 +1,67 @@
+import React, { useState, useEffect } from "react";
 import { jobsIndexRequest } from "../utilities/jobs-api";
-import { useEffect, useState } from "react";
 import JobList from "./components/JobList/JobList";
 
-export default function JobListPage(){
-
+export default function JobListPage() {
   const [jobs, setJobs] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1); // Initial page 1
+  const itemsPerPage = 5;
+
   useEffect(() => {
     async function getJobs() {
-      const jobs = jobsIndexRequest();
-      setJobs(jobs);
+      const fetchedJobs = await jobsIndexRequest();
+      setJobs(fetchedJobs);
     }
     getJobs();
-  }, [])
+  }, []);
+
+  const handleDecrement = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleIncrement = () => {
+    const maxPages = Math.ceil(jobs.length / itemsPerPage); // total pages
+    if (currentPage < maxPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const getVisibleJobs = () => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return jobs.slice(startIndex, endIndex);
+  };
 
   return (
     <>
-    <main className="mt-6 text-center">
-      add filter functionality //
-      pagify and add appropriate route //
-      filters?
-      <JobList jobs={jobs}>
-      </JobList>
-    </main>
+      <main className="mt-6 text-center">
+        {/* Filter functionality (add your implementation here) */}
+        <JobList jobs={getVisibleJobs()} />
+
+        <div className="flex flex-row mx-auto mt-4">
+          <button
+            className="ml-auto dec cursor-pointer"
+            type="button"
+            disabled={currentPage === 1}
+            onClick={handleDecrement}
+          >
+            <span>&lt;</span>
+          </button>
+          <div className="px-2">
+            {currentPage}
+          </div>
+          <button
+            className="mr-auto inc cursor-pointer"
+            type="button"
+            disabled={currentPage === Math.ceil(jobs.length / itemsPerPage)}
+            onClick={handleIncrement}
+          >
+            <span>&gt;</span>
+          </button>
+        </div>
+      </main>
     </>
   );
 }
-

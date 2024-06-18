fd8b49958efdb3923231b51443b690f61437301b
chasehaye <chasehayejoc@gmail.com>
2024-06-17 21:39:56 -0700

    heroku

--- server.js
+++ server.js
@@ -21,11 +21,20 @@ app.use('/api/jobs', require('./routes/api/jobs'));
 app.use('/api/tech', require('./routes/api/technologies'));
 
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
   });
+}else{
+  app.get('/', function(req, res) {
+    res.send('api online');
+  });
+}
   
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log(`Express app running on port ${port}`)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
